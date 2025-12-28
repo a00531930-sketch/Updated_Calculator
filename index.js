896acb759a2ccd287a89b63264d0a52a6269c428
Angel <a00531930@gmail.com>
2025-12-28 09:06:00 +0530

    add squareroot and percentage

--- index.js
+++ index.js
@@ -11,8 +11,23 @@ for (item of button){
     })
 }
 function Calculation(){
+    if (input_data.includes('%'))
+    {
+      let arr_data=input_data.split('%')
+      let a=parseInt(arr_data[0])
+      let b=parseInt(arr_data[1])
+      let per=(a*b)/100
+      ans.value=per
+    }else if (input_data.includes('√'))
+    {
+        let a=parseInt(input_data.substring(1))
+        let sqr=Math.sqrt(a)
+        ans.value=sqr
+    }
+    else{
     var result=eval(input_data)
     ans.value=result
+    }
 }
 function Reset()
 {
